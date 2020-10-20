97b2b8043b27cd570ea55a284ee1bdf712ff6ca6
Javier Botero <javierbotero10271@gmail.com>
2020-10-20 11:43:57 -0500

    Add arrow functions to greeting module

--- src/greeting.js
+++ src/greeting.js
@@ -1,11 +1,10 @@
 import displayMenus from './menu';
 import displayWines from './wines';
-
-const { default: displayHome } = require('./home');
+import displayHome from './home';
 
 const content = document.getElementById('content');
 
-function menusContent() {
+const menusContent = () => {
   let html = '';
   let i = 0;
   ['Home', 'Menu', 'Wines'].forEach((content) => {
@@ -13,9 +12,9 @@ function menusContent() {
     i += 1;
   });
   return html;
-}
+};
 
-function greeting() {
+const greeting = () => {
   document.body.className = 'container-fluid';
   const myImage = document.createElement('div');
   myImage.classList += 'opening';
@@ -31,14 +30,14 @@ function greeting() {
     <h3 class="text-center py-5">Welcome to the flavors of the Caribbean</h3>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </div>`;
-}
+};
 
-function makeClickableMenu() {
-  const array = [displayHome, displayMenus, displayWines];
+const makeClickableMenu = () => {
+  const array = [displayHome, () => displayMenus(), displayWines];
   for (let i = 0; i < 3; i += 1) {
     document.getElementById(i).addEventListener('click', array[i]);
   }
-}
+};
 
 export {
   greeting,
